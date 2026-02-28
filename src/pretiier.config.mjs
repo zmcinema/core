7a5ae285ed106ba383cba930f0d61ae90c7ad6c3
MashinaDonnu <43819117+MashinaDonnu@users.noreply.github.com>
2026-02-28 17:29:46 +0200

    init

---
+++ src/pretiier.config.mjs
@@ -0,0 +1,26 @@
+export default {
+    "trailingComma": "none",
+    "tabWidth": 4,
+    "useTabs": true,
+    "semi": false,
+    "singleQuote": true,
+    "jsxSingleQuote": true,
+    "arrowParens": "avoid",
+    "importOrderSeparation": true,
+    "importOrderSortSpecifiers": true,
+    "importOrderCaseInsensitive": true,
+    "importOrderParserPlugins": [
+        "classProperties",
+        "decorators-legacy",
+        "typescript"
+    ],
+    "importOrder": [
+        "<THIRD_PARTY_MODULES>",
+        "^@/(.*)$",
+        "^[.]/(.*)",
+        "^../(.*)"
+    ],
+    "plugins": [
+        "@trivago/prettier-plugin-sort-imports"
+    ]
+}
